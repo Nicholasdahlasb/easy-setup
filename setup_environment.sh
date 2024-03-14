6779c05800a8ccd96598da550c2a245bf2367e79
Nicholasdahlasb <114733159+Nicholasdahlasb@users.noreply.github.com>
2024-03-14 14:36:33 +0100

    Update setup_environment.sh

--- setup_environment.sh
+++ setup_environment.sh
@@ -1,8 +1,6 @@
 sudo pacman -Syu --noconfirm
-sudo pacman -S --needed base-devel git --noconfirm
+sudo pacman -S --needed --noconfirm base-devel git
 sudo pacman -S python python-pip --noconfirm
-yay -S --needed pycharm-community --noconfirm
-sudo pacman -S --needed vim nano gcc make cmake wget curl nodejs npm ruby go rust jdk-openjdk kotlin perl lua php racket ghc cabal-install erlang elixir haskell-stack ocaml opam scala swift vala dotnet-sdk julia perl6-raku crystal clojure groovy dart fish postgresql mariadb sqlite mongodb redis --noconfirm
-git config --global user.name "Your Name"
-git config --global user.email "youremail@example.com"
+yay -S --needed --noconfirm pycharm-community 
+sudo pacman -S --needed --noconfirm vim nano gcc make cmake wget curl nodejs npm ruby go rust jdk-openjdk kotlin perl lua php racket ghc cabal-install erlang elixir haskell-stack ocaml opam scala swift vala dotnet-sdk julia perl6-raku crystal clojure groovy dart fish postgresql mariadb sqlite mongodb redis
 echo "Development environment setup complete!"
